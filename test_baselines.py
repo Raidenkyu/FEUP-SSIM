7e5cc998b602ca0f3c69449e557089b7a1b77dc4
João Carlos Maduro <joao-carlos.14@hotmail.com>
2020-11-27 19:48:07 +0000

    Added initial setup

--- test_baselines.py
+++ test_baselines.py
@@ -2,7 +2,7 @@ import os
 import retro
 from stable_baselines3 import PPO
 from stable_baselines3.ppo.policies import CnnPolicy
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,13 +13,22 @@ retro.data.Integrations.add_custom_path(
 env = retro.make('SonicTheHedgehog-Genesis',
                  state='act1.state', inttype=retro.data.Integrations.ALL)
 
+video_folder = 'logs/videos/'
+video_length = 10000000
+
 env = DummyVecEnv([lambda: env])
 
-model = PPO(policy=CnnPolicy, env=env, verbose=1)
+env = VecVideoRecorder(env, video_folder,
+                       record_video_trigger=lambda x: x == 0, video_length=video_length,
+                       name_prefix="test-sonic")
+
+model = PPO(policy=CnnPolicy, env=env, verbose=1, device='cpu')
 model.learn(total_timesteps=10000)
 
 obs = env.reset()
-while True:
+for _ in range(video_length + 1):
     action, _info = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     env.render()
+
+env.close()
