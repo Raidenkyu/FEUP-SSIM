43d65a6b733b5e6ff824537410dc608ebd83d2cd
FernandoJAlves <fjcbca@gmail.com>
2020-12-10 18:47:24 +0000

    Code refactor to follow standards

--- res/SonicTheHedgehog-Genesis/script.lua
+++ res/SonicTheHedgehog-Genesis/script.lua
@@ -1,34 +1,35 @@
-level_max_x = 0x2560
+Level_max_x = 0x2560
 
-prev_lives = 3
+Prev_lives = 3
 
-function done()
-    if data.lives < prev_lives then return true end
-    prev_lives = data.lives
+function Done()
+    if data.lives < Prev_lives then return true end
+    Prev_lives = data.lives
 
-    if calc_progress(data) >= 1 then return true end
+    if Calc_progress(data) >= 1 then return true end
 
     return false
 end
 
-prev_progress = 0
-frame_count = 0
-frame_limit = 18000
+Prev_progress = 0
+Frame_count = 0
+Frame_limit = 18000
 
-function reward()
-    frame_count = frame_count + 1
-    local progress = calc_progress(data)
-    local reward = (progress - prev_progress) * 9000
-    prev_progress = progress
+function Reward()
+    Frame_count = Frame_count + 1
+    local progress = Calc_progress(data)
+    local reward = (progress - Prev_progress) * 9000
+    Prev_progress = progress
 
     -- bonus for beating level quickly
     if progress >= 1 then
-        reward = reward + (1 - clip(frame_count / frame_limit, 0, 1)) * 1000
+        reward = reward + (1 - Normalize(Frame_count / Frame_limit, 0, 1)) * 1000
     end
     return reward
 end
 
-function clip(v, min, max)
+-- normalizes the value "v" to be between [min, max]
+function Normalize(v, min, max)
     if v < min then
         return min
     elseif v > max then
@@ -38,18 +39,18 @@ function clip(v, min, max)
     end
 end
 
-offset_x = nil
-end_x = nil
+Offset_x = nil
+End_x = nil
 
-function calc_progress(data)
-    if offset_x == nil then
-        offset_x = -data.x
-        local key = string.format("zone=%d,act=%d", data.zone, data.act)
-        end_x = level_max_x - data.x
+-- returns a percentage of completion between [0, 1]
+function Calc_progress(data)
+    if Offset_x == nil then
+        Offset_x = -data.x
+        End_x = Level_max_x - data.x
     end
 
-    local cur_x = clip(data.x + offset_x, 0, end_x)
-    return cur_x / end_x
+    local cur_x = Normalize(data.x + Offset_x, 0, End_x)
+    return cur_x / End_x
 end
 
 
