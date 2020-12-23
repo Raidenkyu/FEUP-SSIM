85ab16f50602357b907a5ae4110306dcdba38f8c
FernandoJAlves <fjcbca@gmail.com>
2020-12-23 16:08:47 +0000

    Refactor to reward function to now depend on the max_x and not the current position

--- res/SonicTheHedgehog-Genesis/script.lua
+++ res/SonicTheHedgehog-Genesis/script.lua
@@ -23,6 +23,7 @@ Prev_progress = 0
 Frame_count = 0
 Frame_limit = 18000 -- 36102 is the 10min timeout
 Total_reward = 0
+Max_x = 0
 
 function Reward()
 
@@ -32,6 +33,7 @@ function Reward()
         return -Total_reward - 0.5
     end
 
+    Update_max()
     local new_reward = Get_reward()
     Total_reward = Total_reward + new_reward
 
@@ -39,16 +41,12 @@ function Reward()
 
 end
 
-function Get_reward( ... )
-
-    -- TODO: this is just to incentivise faster runs, needs to be reviewed, may be preventing passing the loop
-    -- if Is_stuck(data) then
-    --     return -0.5
-    -- end
+function Get_reward()
 
     Frame_count = Frame_count + 1
-    local new_progress = Calc_progress(data)
-    local reward = (new_progress - Prev_progress) * 100
+    -- local new_progress = Calc_progress(data)
+    local new_progress = Calc_progress_max()
+    local reward = new_progress
 
     Prev_progress = new_progress
 
@@ -58,6 +56,17 @@ function Get_reward( ... )
         reward = reward + (1 - Normalize(Frame_count / Frame_limit, 0, 1)) * 1000
     end
     return reward
+end
+
+function Update_max()
+    local new_max = math.max(Max_x, data.x + Offset_x)
+
+    -- FOR DEBUG
+    if new_max > Max_x then
+        -- Print_tab("new_max: " .. new_max)
+    end
+
+    Max_x = new_max
 end
 
 -- normalizes the value "v" to be between [min, max]
@@ -74,7 +83,7 @@ end
 Offset_x = nil
 End_x = nil
 
--- returns a percentage of completion between [0, 1]
+-- returns a percentage of completion between [0, 1] (DEPRECATED)
 function Calc_progress(data)
     if Offset_x == nil then
         Offset_x = -data.x
@@ -86,25 +95,11 @@ function Calc_progress(data)
     return ret_value
 end
 
-Max_x = 0
-Frames_since_last_max = 0
-
--- returns true if the agent has not progressed in a while
-function Is_stuck(data)
-
-    local new_max = math.max(Max_x, data.x + Offset_x)
-
-    if new_max > Max_x then
-        Frames_since_last_max = 0
-    else
-        Frames_since_last_max = Frames_since_last_max + 1
-    end
-    Max_x = new_max
-
-    local is_stuck = false
-    if Frames_since_last_max > 1000 then is_stuck = true end
-
-    return is_stuck
+-- returns a percentage of completion between [0, 1] (using Max_x)
+function Calc_progress_max()
+    local cur_x = Normalize(Max_x, 0, End_x)
+    local ret_value = cur_x / End_x
+    return ret_value
 end
 
 -- Print functions, for debugging
